4845274b9a4fee549bc23b87af0c2e407b593dae
ouzhibing <ouzhibing@galaxymx.com>
2018-09-27 10:19:04 +0800

    获取灯塔文章列表

--- app/Http/Controllers/Wechat/CollegeArticleController.php
+++ app/Http/Controllers/Wechat/CollegeArticleController.php
@@ -22,26 +22,58 @@ class CollegeArticleController extends Controller
         $this->collegeService = $collegeArticleService;
     }
 
+    /**
+     * 灯塔文章列表
+     *
+     * @author yezi
+     * @return array
+     */
     public function article()
     {
-        $result = CollegeArticle::orderBy(CollegeArticle::FIELD_CREATED_AT,'desc')
-            ->select([
-                CollegeArticle::FIELD_ID,
-                CollegeArticle::FIELD_ID_POSTER,
-                CollegeArticle::FIELD_COVER_IMAGE,
-                CollegeArticle::FIELD_TITLE,
-                CollegeArticle::FIELD_CREATED_AT
-            ])
-            ->get();
+        $orderBy = request()->input('order_by', 'created_at');
+        $sortBy = request()->input('sort_by', 'desc');
+        $filter = request()->input('filter');
+        $pageSize = request()->input('page_size', 10);
+        $pageNumber = request()->input('page_number', 1);
+        $type = CollegeArticle::ENUM_COLLEGE_NOTE;
+
+        $pageParams = ['page_size' => $pageSize, 'page_number' => $pageNumber];
+
+        $selectData = [
+            CollegeArticle::FIELD_ID,
+            CollegeArticle::FIELD_ID_POSTER,
+            CollegeArticle::FIELD_COVER_IMAGE,
+            CollegeArticle::FIELD_TITLE,
+            CollegeArticle::FIELD_CREATED_AT
+        ];
+        $query = $this->collegeService->getBuilder()->filter($type)->sort($orderBy,$sortBy)->done();
+        $result = paginate($query,$pageParams,$selectData,function ($item){
+            return $item;
+        });
 
         return $result;
     }
 
+    /**
+     * 文章详情
+     *
+     * @author yezi
+     * @param $id
+     * @return \Illuminate\Database\Eloquent\Model|null|object|static
+     */
     public function detail($id)
     {
-        return CollegeArticle::query()->where(CollegeArticle::FIELD_ID,$id)->first();
+        $result = CollegeArticle::query()->where(CollegeArticle::FIELD_ID,$id)->first();
+
+        return $result;
     }
 
+    /**
+     * 笔记列表
+     *
+     * @author yezi
+     * @return array
+     */
     public function noteList()
     {
         $orderBy = request()->input('order_by', 'created_at');
--- app/Http/Service/CollegeArticleService.php
+++ app/Http/Service/CollegeArticleService.php
@@ -16,6 +16,17 @@ class CollegeArticleService
 {
     private $builder;
 
+    /**
+     * 新建文章
+     *
+     * @author yezi
+     * @param $userId
+     * @param $title
+     * @param $content
+     * @param $cover
+     * @param $type
+     * @return mixed
+     */
     public function save($userId,$title,$content,$cover,$type)
     {
         $result = Model::create([
@@ -29,6 +40,12 @@ class CollegeArticleService
         return $result;
     }
 
+    /**
+     * 构建查询构造器
+     *
+     * @author yezi
+     * @return $this
+     */
     public function getBuilder()
     {
         $this->builder = Model::query();
@@ -36,12 +53,27 @@ class CollegeArticleService
         return $this;
     }
 
+    /**
+     * 过滤查询
+     *
+     * @author yezi
+     * @param $type
+     * @return $this
+     */
     public function filter($type){
         $this->builder->where(Model::FIELD_TYPE,$type);
 
         return $this;
     }
 
+    /**
+     * 排序
+     *
+     * @author yezi
+     * @param $orderBy
+     * @param $sort
+     * @return $this
+     */
     public function sort($orderBy,$sort)
     {
         $this->builder->orderBy($orderBy,$sort);
@@ -49,11 +81,24 @@ class CollegeArticleService
         return $this;
     }
 
+    /**
+     * 构造查询语句结束
+     *
+     * @author yezi
+     * @return mixed
+     */
     public function done()
     {
         return $this->builder;
     }
 
+    /**
+     * 格式化单挑数据
+     *
+     * @author yezi
+     * @param $item
+     * @return mixed
+     */
     public function formatSingle($item)
     {
         $item->title_length = strlen($item->{CollegeArticle::FIELD_TITLE});
